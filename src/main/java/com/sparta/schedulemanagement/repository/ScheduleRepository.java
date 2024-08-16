81a876c8e08490bcfce01f9d79f87c134327f716
Jiyun <jiyumi00@naver.com>
2024-08-16 11:30:06 +0900

    feat : Repository 역할 분리
    3Layer Architecture

---
+++ src/main/java/com/sparta/schedulemanagement/repository/ScheduleRepository.java
@@ -0,0 +1,88 @@
+package com.sparta.schedulemanagement.repository;
+
+import com.sparta.schedulemanagement.dto.ScheduleRequestDto;
+import com.sparta.schedulemanagement.dto.ScheduleResponseDto;
+import com.sparta.schedulemanagement.entity.Schedule;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class ScheduleRepository {
+    private final JdbcTemplate jdbcTemplate;
+    public ScheduleRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate=jdbcTemplate;
+    }
+
+    public Schedule save(Schedule schedule) {
+        KeyHolder keyHolder=new GeneratedKeyHolder(); //기본 키를 반환받기 위한 객체
+
+        String sql="INSERT INTO schedule (name, password, todo, date) VALUES (?,?,?,?)";
+
+        jdbcTemplate.update(con ->{
+            PreparedStatement preparedStatement=con.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1,schedule.getName());
+            preparedStatement.setString(2,schedule.getPassword());
+            preparedStatement.setString(3,schedule.getTodo());
+            preparedStatement.setString(4,schedule.getDate());
+
+            return preparedStatement;
+        },keyHolder);
+
+        Long id=keyHolder.getKey().longValue();
+        schedule.setId(id);
+        return schedule;
+    }
+
+    public Schedule findById(Long id){
+        //DB 조회
+        String sql="SELECT * FROM schedule WHERE id = ?";
+
+        return jdbcTemplate.query(sql,resultSet ->{
+            if(resultSet.next()){
+                Schedule schedule=new Schedule();
+                schedule.setName(resultSet.getString("name"));
+                schedule.setTodo(resultSet.getString("todo"));
+                return schedule;
+            }else{
+                return null;
+            }
+        },id);
+    }
+
+    public List<ScheduleResponseDto> findAll() {
+        //DB 조회
+        String sql="SELECT * FROM schedule";
+
+        return jdbcTemplate.query(sql, new RowMapper<ScheduleResponseDto>() {
+            @Override
+            public ScheduleResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Long id=rs.getLong("id");
+                String name=rs.getString("name");
+                String todo=rs.getString("todo");
+                String date=rs.getString("date");
+                return new ScheduleResponseDto(id,name,todo,date);
+            }
+        });
+    }
+
+    public void delete(Long id) {
+        String sql="DELETE FROM schedule WHERE id = ?";
+        jdbcTemplate.update(sql,id);
+    }
+
+    public void updateSchedule(Long id, ScheduleRequestDto scheduleRequestDto) {
+        String sql="UPDATE schedule SET name = ?, todo = ? WHERE id = ?";
+        jdbcTemplate.update(sql,scheduleRequestDto.getName(),scheduleRequestDto.getTodo(),id);
+    }
+
+
+}
--- src/main/java/com/sparta/schedulemanagement/service/ScheduleService.java
+++ src/main/java/com/sparta/schedulemanagement/service/ScheduleService.java
@@ -3,15 +3,12 @@ package com.sparta.schedulemanagement.service;
 import com.sparta.schedulemanagement.dto.ScheduleRequestDto;
 import com.sparta.schedulemanagement.dto.ScheduleResponseDto;
 import com.sparta.schedulemanagement.entity.Schedule;
+import com.sparta.schedulemanagement.repository.ScheduleRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 public class ScheduleService {
@@ -24,25 +21,10 @@ public class ScheduleService {
     public ScheduleResponseDto createSchedule(ScheduleRequestDto requestDto) {
         //RequestDto -> Entity
         Schedule schedule=new Schedule(requestDto);
+
         //DB 저장
-        KeyHolder keyHolder=new GeneratedKeyHolder(); //기본 키를 반환받기 위한 객체
-
-        String sql="INSERT INTO schedule (name, password, todo, date) VALUES (?,?,?,?)";
-
-        jdbcTemplate.update(con ->{
-            PreparedStatement preparedStatement=con.prepareStatement(sql,
-                    Statement.RETURN_GENERATED_KEYS);
-
-            preparedStatement.setString(1,schedule.getName());
-            preparedStatement.setString(2,schedule.getPassword());
-            preparedStatement.setString(3,schedule.getTodo());
-            preparedStatement.setString(4,schedule.getDate());
-
-            return preparedStatement;
-        },keyHolder);
-
-        Long id=keyHolder.getKey().longValue();
-        schedule.setId(id);
+        ScheduleRepository scheduleRepository=new ScheduleRepository(jdbcTemplate);
+        Schedule saveSchedule=scheduleRepository.save(schedule);
 
         //Entity -> ResponseDto
         ScheduleResponseDto scheduleResponseDto=new ScheduleResponseDto(schedule);
@@ -70,7 +52,38 @@ public class ScheduleService {
         }
     }
 
-    private Schedule findById(Long id){
+
+
+    public Long updateSchedule(Long id, ScheduleRequestDto scheduleRequestDto) {
+        //해당 일정이 DB에 존재하는 지 확인
+        ScheduleRepository scheduleRepository=new ScheduleRepository(jdbcTemplate);
+        Schedule schedule=scheduleRepository.findById(id);
+        if(schedule!=null){
+           scheduleRepository.updateSchedule(id,scheduleRequestDto);
+            return id;
+        }else{
+            throw new IllegalArgumentException("선택한 일정은 존재하지 않습니다");
+        }
+    }
+
+    public List<ScheduleResponseDto> getScheduleList() {
+        ScheduleRepository scheduleRepository=new ScheduleRepository(jdbcTemplate);
+        return scheduleRepository.findAll();
+    }
+
+    public Long deleteSchedule(Long id) {
+        ScheduleRepository scheduleRepository=new ScheduleRepository(jdbcTemplate);
+
+        Schedule schedule=scheduleRepository.findById(id);
+        //해당 일정이 DB에 존재하는 지 확인
+        if(schedule!=null){
+            scheduleRepository.delete(id);
+            return id;
+        }else{
+            throw new IllegalArgumentException("선택한 일정은 존재하지 않습니다");
+        }
+    }
+    public Schedule findById(Long id){
         //DB 조회
         String sql="SELECT * FROM schedule WHERE id = ?";
 
@@ -84,48 +97,5 @@ public class ScheduleService {
                 return null;
             }
         },id);
-    }
-
-    public Long updateSchedule(Long id, ScheduleRequestDto scheduleRequestDto) {
-        //해당 일정이 DB에 존재하는 지 확인
-        Schedule schedule=findById(id);
-        if(schedule!=null){
-            String sql="UPDATE schedule SET name = ?, todo = ? WHERE id = ?";
-            jdbcTemplate.update(sql,scheduleRequestDto.getName(),scheduleRequestDto.getTodo(),id);
-
-            //일정 수정
-            return id;
-        }else{
-            throw new IllegalArgumentException("선택한 일정은 존재하지 않습니다");
-        }
-    }
-
-    public List<ScheduleResponseDto> getScheduleList() {
-        //DB 조회
-        String sql="SELECT * FROM schedule";
-
-        return jdbcTemplate.query(sql, new RowMapper<ScheduleResponseDto>() {
-            @Override
-            public ScheduleResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Long id=rs.getLong("id");
-                String name=rs.getString("name");
-                String todo=rs.getString("todo");
-                String date=rs.getString("date");
-                return new ScheduleResponseDto(id,name,todo,date);
-            }
-        });
-    }
-
-    public Long deleteSchedule(Long id) {
-        Schedule schedule=findById(id);
-        //해당 일정이 DB에 존재하는 지 확인
-        if(schedule!=null){
-            String sql="DELETE FROM schedule WHERE id = ?";
-            jdbcTemplate.update(sql,id);
-
-            return id;
-        }else{
-            throw new IllegalArgumentException("선택한 일정은 존재하지 않습니다");
-        }
     }
 }
